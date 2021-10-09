e079114753179cfb93705266198e543a5128f225
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-09 11:14:08 +0530

    Update logo.py

--- Sophia/modules/logo.py
+++ Sophia/modules/logo.py
@@ -102,7 +102,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="gold", stroke_width=0, stroke_fill="gold")
     fname2 = "SophiaLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @SophiaSLBot 💞")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Devil_the_Robot 😈")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -142,11 +142,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "SophiaLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @SophiaSLBot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Devil_the_Robot 😈")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Go to Help From  @Dihan_Official, {e}')
+   await event.reply(f'Error Go to Help From  @PigasusSupport, {e}')
 
 
 
@@ -186,7 +186,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Go to Help From  @Dihan_Official, {e}')
+   await event.reply(f'Error Go to Help From  @PigasusSupport, {e}')
 
 
 
@@ -202,4 +202,4 @@ __help__ = """
  ~ /wlogo {text} :  Create your White Logo with your name
  ~ /rlogo {text} :  Create your Red logo with your name
  """
-__mod_name__ = "💞Logo💞"
+__mod_name__ = "😈Logo😈"
