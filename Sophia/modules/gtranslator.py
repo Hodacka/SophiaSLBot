f530bddc5c42af12d6eefff26ea984548228044b
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:40:25 +0530

    Update gtranslator.py

--- Sophia/modules/gtranslator.py
+++ Sophia/modules/gtranslator.py
@@ -109,6 +109,6 @@ TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "💞G-Trans💞"
+__mod_name__ = "😈G-Trans😈"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
