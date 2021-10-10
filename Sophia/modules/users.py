3889b3bad36d29ac0fe09017c747da4dbf380bfb
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:58:24 +0530

    Update users.py

--- Sophia/modules/users.py
+++ Sophia/modules/users.py
@@ -187,5 +187,5 @@ dispatcher.add_handler(BROADCAST_HANDLER)
 dispatcher.add_handler(CHATLIST_HANDLER)
 dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
 
-__mod_name__ = "💞Users💞"
+__mod_name__ = "😈Users😈"
 __handlers__ = [(USER_HANDLER, USERS_GROUP), BROADCAST_HANDLER, CHATLIST_HANDLER]
