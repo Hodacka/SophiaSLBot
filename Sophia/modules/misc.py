5584ce66411e3b59ff23e80617056afd09c37e36
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:43:42 +0530

    Update misc.py

--- Sophia/modules/misc.py
+++ Sophia/modules/misc.py
@@ -131,7 +131,7 @@ MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "💞Extras💞"
+__mod_name__ = "😈Extras😈"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER,
