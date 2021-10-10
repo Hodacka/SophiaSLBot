21361f8d1ee20d6acdc40a308dd60e69aa0cb689
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:45:07 +0530

    Update muting.py

--- Sophia/modules/muting.py
+++ Sophia/modules/muting.py
@@ -254,5 +254,5 @@ dispatcher.add_handler(MUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 
-__mod_name__ = "💞Muting💞"
+__mod_name__ = "😈Muting😈"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
