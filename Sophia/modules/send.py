b9cc6567dc8d9af13b21399310dcf14bb3de45be
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:50:09 +0530

    Update send.py

--- Sophia/modules/send.py
+++ Sophia/modules/send.py
@@ -23,7 +23,7 @@ __help__ = """The Send Module Allows you to send a custom message to users in a 
 /snd :Send the given message
 Note - /snd Hi will send the message hi to the chat"""
 
-__mod_name__ = "💞Send💞"
+__mod_name__ = "😈Send😈"
 
 
 ADD_CCHAT_HANDLER = DisableAbleCommandHandler("snd", send)
