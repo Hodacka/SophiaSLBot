d5a8ff5f6af3c9573d539f0e6f1d302a329e7175
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:47:16 +0530

    Update purge.py

--- Sophia/modules/purge.py
+++ Sophia/modules/purge.py
@@ -77,6 +77,6 @@ DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
 
-__mod_name__ = "💞Purge💞"
+__mod_name__ = "😈Purge😈"
 __command_list__ = ["del", "purge"]
 __handlers__ = [PURGE_HANDLER, DEL_HANDLER]
