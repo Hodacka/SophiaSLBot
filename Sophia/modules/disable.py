255b0d9661591bdcd2ff5a132532b82abdd46cb2
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:25:42 +0530

    Update disable.py

--- Sophia/modules/disable.py
+++ Sophia/modules/disable.py
@@ -347,7 +347,7 @@ if is_module_loaded(FILENAME):
     ~ /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "💞Disable💞"
+    __mod_name__ = "😈Disable😈"
 
 else:
     DisableAbleCommandHandler = CommandHandler
