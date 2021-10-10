46228f42a2b1c5af4a25dc76d8e193b0a0f3a458
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-10 12:20:47 +0530

    Update cleaner.py

--- Sophia/modules/cleaner.py
+++ Sophia/modules/cleaner.py
@@ -242,7 +242,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "💞Cleaner💞"
+__mod_name__ = "😈Cleaner😈"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
